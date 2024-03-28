c84dc192cbd399f12d845e34aac8ce0ba525af96
DuyVu2511 <duyvutran2004@gmail.com>
2024-03-28 20:22:12 +0700

    JsApp

---
+++ Trần Duy Vũ - K22CNT3/src/App.js
@@ -0,0 +1,18 @@
+import logo from './Dai_hoc_NguyenTrai.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Đại học Nguyễn Trãi
+        </p>
+        <h1>Trần Duy Vũ - CNT3 - 2210900138</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
